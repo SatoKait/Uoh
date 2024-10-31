d110e7304827d4f245de4da250b84cb010d78abb
Tomoki Yoshida <zeal23300042@fsgcl.jp>
2024-10-31 09:43:27 +0900

    要らないものを消しCSVの配置を変えた

--- GameSources/Block.cpp
+++ GameSources/Block.cpp
@@ -14,18 +14,12 @@ namespace basecross {
 		auto texturePath = mediaPath + L"Textures/"; // 「Textures/」フォルダのパスを連結する
 		app->RegisterTexture(L"block", texturePath + L"block.jpg");
 
+
 		auto col = AddComponent<CollisionObb>();
 		col->SetDrawActive(true);
 		col->SetFixed(true);
 		col->SetSleepActive(true);
 
-		//Mat4x4 spanMat; // モデルとトランスフォーム間の差分行列
-		//spanMat.affineTransformation(
-		//	Vec3(1.0f, 1.0f, 1.0f),//スケーリング
-		//	Vec3(0.0f, 0.0f, 0.0f),//回転の中心
-		//	Vec3(0.0f, 0.0f, 0.0f),//回転のベクトル
-		//	Vec3(0.0f, 0.0f, 0.0f) //移動
-		//);
 
 		auto drawComp = AddComponent<PNTStaticInstanceDraw>(); //Instanceと付いたドローコンポーネントを生成します
 		drawComp->SetMeshResource(m_meshResName);
