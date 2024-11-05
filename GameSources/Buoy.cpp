38a9849964767fd31a7e8f2dc419a033d1597df7
Tomoki Yoshida <zeal23300042@fsgcl.jp>
2024-11-05 10:21:32 +0900

    h.cppを追加した

---
+++ GameSources/Buoy.cpp
@@ -0,0 +1,31 @@
+/*!
+@file Buoy.cpp
+@brief 障害物など実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	void Buoy::OnCreate() {
+		auto& app = App::GetApp();//アプリケーションの参照
+		auto mediaPath = app->GetDataDirWString(); // 「media」パスを文字列として取得する
+		auto texturePath = mediaPath + L"Textures/"; // 「Textures/」フォルダのパスを連結する
+		app->RegisterTexture(L"block", texturePath + L"block.jpg");
+
+		//auto col = AddComponent<CollisionObb>();
+		//col->SetDrawActive(true);
+		//col->SetFixed(true);
+		//col->SetSleepActive(true);
+
+		auto drawComp = AddComponent<PNTStaticInstanceDraw>(); //Instanceと付いたドローコンポーネントを生成します
+		drawComp->SetMeshResource(m_meshResName);
+		drawComp->SetTextureResource(m_Reskey);
+		drawComp->SetOwnShadowActive(true);
+
+		auto shadowComp = AddComponent<Shadowmap>();
+		shadowComp->SetMeshResource(m_meshResName);
+
+	}
+}
+//end basecross
---
+++ GameSources/Buoy.h
@@ -0,0 +1,30 @@
+#pragma once
+/*!
+@file Buoy.h
+@brief É|Å[ÉãÇÃíËã`
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class Buoy : public GameObject
+	{
+		shared_ptr<Transform>m_ptrTrans;
+		wstring m_meshResName;
+		wstring m_Reskey;
+		Vec3 m_Scale;
+
+	public:
+		Buoy(const std::shared_ptr<Stage>& stage) :
+			GameObject(stage),
+			m_meshResName(L"POLL_MESH"),
+			m_Scale(0.1f),
+			m_Reskey(L"RED_TX")
+		{
+		}
+
+		void OnCreate();
+	};
+
+}
