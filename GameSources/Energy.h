a9228285e52b5295086a9186036be4ccb27c1423
Tomoki Yoshida <zeal23300042@fsgcl.jp>
2024-11-05 10:22:05 +0900

    名前の変更をした

--- GameSources/Energy.h
+++ GameSources/Energy.h
@@ -9,7 +9,7 @@ namespace basecross {
 	//--------------------------------------------------------------------------------------
 	///	スクロールするスプライト
 	//--------------------------------------------------------------------------------------
-	class ScrollSprite : public GameObject {
+	class Energy : public GameObject {
 		bool m_Trace;
 		Vec2 m_StartScale;
 		Vec3 m_StartPos;
@@ -22,9 +22,9 @@ namespace basecross {
 		shared_ptr<PTSpriteDraw>m_ptrDraw;
 
 	public:
-		ScrollSprite(const shared_ptr<Stage>& StagePtr, const wstring& TextureKey, bool Trace,
+		Energy(const shared_ptr<Stage>& StagePtr, const wstring& TextureKey, bool Trace,
 			const Vec2& StartScale, const Vec3& StartPos);
-		virtual ~ScrollSprite();
+		virtual ~Energy();
 		virtual void OnCreate() override;
 		virtual void OnUpdate()override;
 	};
