ae8742b96e42db07e2416d23f2c68c010433c025
Tomoki Yoshida <zeal23300042@fsgcl.jp>
2024-12-02 12:09:53 +0900

    スコアの出し方の変更

--- GameSources/Score.cpp
+++ GameSources/Score.cpp
@@ -11,6 +11,59 @@ namespace basecross {
 	//初期化
 	void Score::OnCreate()
 	{
+		//m_Trans = GetComponent<Transform>();
+		//m_Trans->SetPosition(m_Position);
+
+
+
+		const int numPlaces = 5;
+		m_numberSprites.reserve(numPlaces);
+		int place = 10000;
+
+		for (int i = 0; i < numPlaces; i++)
+		{
+			auto stage = GetStage();
+			number = stage->AddGameObject<ScoreSprite>();
+			m_numberTrans = number->GetComponent<Transform>();
+			m_numberTrans->SetPosition(-50 * 0.5f - 50.0f * (numPlaces - i), 800 * 0.5f, 0.0f);
+			number->UpdateValue(m_score / place % 10);
+			place /= 10;
+			m_numberSprites.push_back(number);
+		}
+	}
+
+	void Score::OnDraw()
+	{
+		//200 * 0.5f - 50.0f * (numPlaces - i), -50 * 0.5f, 0.0f
+		//for (const auto& number : m_numberSprites)
+		//{
+		//	number->OnDraw();
+		//}
+	}
+
+	void Score::OnUpdate()
+	{	auto stage = GetStage();
+	    m_score = App::GetApp()->GetScene<Scene>()->GetScore();//更新で最新の数字を見る
+		int place = 10000;	
+		for (int i = 0; i < m_numberSprites.size(); ++i) {
+			
+			int digit = (m_score / place) % 10;// 現在の桁の数字を計算
+			m_numberSprites[i]->UpdateValue(digit);// スプライトの数字を更新
+			place /= 10;// 次の桁に移動
+		}
+	}
+}
+
+namespace basecross {
+
+	//初期化
+	void StageScore::OnCreate()
+	{
+		//m_Trans = GetComponent<Transform>();
+		//m_Trans->SetPosition(m_Position);
+
+
+
 		const int numPlaces = 5;
 		m_numberSprites.reserve(numPlaces);
 		int place = 10000;
@@ -27,30 +80,44 @@ namespace basecross {
 		}
 	}
 
-	void Score::OnDraw()
+	void StageScore::OnUpdate()
 	{
-		//for (const auto& number : m_numberSprites)
-		//{
-		//	number->OnDraw();
-		//}
-	}
-
-	void Score::OnUpdate()
-	{	
 		auto stage = GetStage();
-		m_score = App::GetApp()->GetScene<Scene>()->GetScore();
+		m_score = App::GetApp()->GetScene<Scene>()->GetScore();//更新で最新の数字を見る
+		int place = 10000;
+		for (int i = 0; i < m_numberSprites.size(); ++i) {
+
+			int digit = (m_score / place) % 10;// 現在の桁の数字を計算
+			m_numberSprites[i]->UpdateValue(digit);// スプライトの数字を更新
+			place /= 10;// 次の桁に移動
+		}
+	}
+}
+
+namespace basecross {
+
+	//初期化
+	void TargetsScore::OnCreate()
+	{
+		//m_Trans = GetComponent<Transform>();
+		//m_Trans->SetPosition(m_Position);
+
+
 
 		const int numPlaces = 5;
 		m_numberSprites.reserve(numPlaces);
 		int place = 10000;
+
 		for (int i = 0; i < numPlaces; i++)
 		{
+			auto stage = GetStage();
+			number = stage->AddGameObject<ScoreSprite>();
 			m_numberTrans = number->GetComponent<Transform>();
-			m_numberTrans->SetPosition(200 * 0.5f - 50.0f * (numPlaces - i), -50 * 0.5f, 0.0f);
+			m_numberTrans->SetPosition(500 * 0.5f - 50.0f * (numPlaces - i), 800 * 0.5f, 0.0f);
 			number->UpdateValue(m_score / place % 10);
 			place /= 10;
 			m_numberSprites.push_back(number);
 		}
-
 	}
 }
+
--- GameSources/Score.h
+++ GameSources/Score.h
@@ -16,39 +16,93 @@ namespace basecross {
 		shared_ptr<ScoreSprite>number;
 
 	public:
-		
 		shared_ptr<Transform> m_numberTrans;
         int m_score;
 		int m_nowScore;
-		int GameScore;
 
 		// 構築と破棄
-		Score(const shared_ptr<Stage>& stage) :
-			GameObject(stage),
+		Score::Score(const shared_ptr<Stage>& StagePtr
+			) :
+			GameObject(StagePtr),
 			m_score(App::GetApp()->GetScene<Scene>()->m_Score),
 			m_nowScore(0)
 		{
 		}
-		virtual ~Score()
-		{
-		}
+
+		Score:: ~Score(){}
 
 		virtual void OnCreate() override; // 初期化
 		virtual void OnDraw() override; // 描画
 		virtual void OnUpdate() override; // 更新
 
 
-		void UpdateScore()
-		{
-			int score = m_score;//スコアをメンバ変数から拝借して
-			//ポリゴンそのものを書き換える事ができない
-			int place = 10000000;
-			for (auto& number : m_numberSprites)
-			{
-				number->UpdateValue(score / place % 10);
-				place /= 10;//スコア自身から10削る//ここが右から削っていくみたいなものなので桁を用意した方が良い
-			}
-		}
+		//void UpdateScore()
+		//{
+		//	int score = m_score;//スコアをメンバ変数から拝借して
+		//	//ポリゴンそのものを書き換える事ができない
+		//	int place = 10000000;
+		//	for (auto& number : m_numberSprites)
+		//	{
+		//		number->UpdateValue(score / place % 10);
+		//		place /= 10;//スコア自身から10削る//ここが右から削っていくみたいなものなので桁を用意した方が良い
+		//	}
+		//}
 	};
+
+	//--------------------------------------------------------------------------------------
+	//　　Stageスコアクラス	
+	//--------------------------------------------------------------------------------------
+	class StageScore : public GameObject
+	{
+		vector<shared_ptr<ScoreSprite>> m_numberSprites;
+		shared_ptr<ScoreSprite>number;
+
+	public:
+		shared_ptr<Transform> m_numberTrans;
+		int m_score;
+		int m_nowScore;
+
+		// 構築と破棄
+		StageScore::StageScore(const shared_ptr<Stage>& StagePtr
+		):
+			GameObject(StagePtr),
+			m_score(App::GetApp()->GetScene<Scene>()->m_Score),
+			m_nowScore(0)
+		{
+		}
+
+		StageScore:: ~StageScore() {}
+
+		virtual void OnCreate() override; // 初期化
+		virtual void OnUpdate() override; // 更新
+	};
+
+	//--------------------------------------------------------------------------------------
+	//　Targetsスコアクラス	
+	//--------------------------------------------------------------------------------------
+	class TargetsScore : public GameObject
+	{
+		vector<shared_ptr<ScoreSprite>> m_numberSprites;
+		shared_ptr<ScoreSprite>number;
+
+	public:
+		shared_ptr<Transform> m_numberTrans;
+		int m_score;
+		int m_nowScore;
+
+		// 構築と破棄
+		TargetsScore::TargetsScore(const shared_ptr<Stage>& StagePtr
+		) :
+			GameObject(StagePtr),
+			m_score(10000)
+		{
+		}
+
+		TargetsScore:: ~TargetsScore() {}
+
+		virtual void OnCreate() override; // 初期化
+	};
+
+
 }
 //end basecross
