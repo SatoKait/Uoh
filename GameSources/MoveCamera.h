76ab0abb052ee0b04ce137a7dc3834fbe6360d1e
Tomoki Yoshida <zeal23300042@fsgcl.jp>
2024-12-03 04:42:57 +0900

    Cameraについての変更

--- GameSources/MoveCamera.h
+++ GameSources/MoveCamera.h
@@ -31,4 +31,58 @@ namespace basecross {
 				m_Owner = ptrCamera;
 			}
    };
+	class MainCamera2 : public Camera
+	{
+		// 注視対象オブジェクトのトランスフォームコンポーネント
+		weak_ptr<GameObject> m_Target;
+
+		// カメラの回り込み（ラジアン）
+		float m_AngleY;
+
+	public:
+		MainCamera2();
+		virtual ~MainCamera2();
+
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+
+		/*!
+		 @brief カメラが注視する対象物を設定する
+		 @param[in] ptrTarget 注視する対象物
+		 @return なし
+		 */
+		void SetTarget(const shared_ptr<GameObject>& ptrTarget)
+		{
+			m_Target = ptrTarget;
+		}
+
+		/*!
+		 @brief カメラが注視する対象物を取得する
+		 @return 注視する対象物
+		 */
+		shared_ptr<GameObject> GetTarget() const
+		{
+			return m_Target.lock();
+		}
+
+		/*!
+		 @brief カメラの回り込みの角度を設定する
+		 @param[in] angle カメラの回り込みの角度（ラジアン角）
+		 @return なし
+		 */
+		void SetAngleY(const float& angle)
+		{
+			m_AngleY = angle;
+		}
+
+		/*!
+		 @brief カメラの回り込みの角度を取得する
+		 @return カメラの回り込みの角度（ラジアン角）
+		 */
+		float GetAngleY() const
+		{
+			return m_AngleY;
+		}
+	};
 }
+//end basecross
