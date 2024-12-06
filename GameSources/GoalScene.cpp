6d822f5d9c481d51b0062cc203ec414fed52f13a
Chinami Ando <zeal23420147@fsgcl.jp>
2024-12-06 14:07:36 +0900

    テクスチャの修正

--- GameSources/GoalScene.cpp
+++ GameSources/GoalScene.cpp
@@ -36,7 +36,7 @@ namespace basecross{
 		AddGameObject<StageSprite>(L"TITLEBACK_TX", true,
 			Vec2(1280.0f, 1080.0f), Vec2(0.0f, 0.0f));
 		AddGameObject<StageSprite>(L"GOALSCENE_TX", true,
-			Vec2(750.0f, 400.0f), Vec2(0.0f, 200.0f));
+			Vec2(1200.0f, 350.0f), Vec2(0.0f, 200.0f));
 		AddGameObject<Flickering>(L"TITLETEXT_TX", true,
 			Vec2(450.0f, 100.0f), Vec2(0.0f, -300.0f));
 
