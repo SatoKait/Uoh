be38ac924f1b6c4278fd5b39b5c1208a1db8cc3d
Chinami Ando <zeal23420147@fsgcl.jp>
2024-12-12 16:10:28 +0900

    GoalSceneにトビウオを追加しアニメーションを再生&王冠を追加

--- GameSources/GoalScene.h
+++ GameSources/GoalScene.h
@@ -9,6 +9,7 @@
 namespace basecross{
 	class GoalScene : public Stage
 	{
+		shared_ptr<GameObject> crown;
 		shared_ptr<SoundItem> m_stageBGM;
 		InputHandler<GoalScene> m_InputHandler;
 
@@ -17,6 +18,10 @@ namespace basecross{
 		void CreateSprite();
 		void CreateBGM();
 
+		float deg;
+		float rad;
+		float m_comY;
+		bool m_flag;
 
 	public:
 		GoalScene() : Stage() {}
@@ -28,6 +33,29 @@ namespace basecross{
 
 	};
 
+	class Model2 : public GameObject {
+		Vec3 m_StartPos;
+		wstring m_TextureUse;
+	public:
+		//\’z‚Æ”jŠü
+		Model2(const shared_ptr<Stage>& StagePtr, const Vec3& StartPos);
+		virtual ~Model2();
+		//‰Šú‰»
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+	};
+
+	class Model3 : public GameObject {
+		Vec3 m_StartPos;
+		Vec3 m_StartRot;
+	public:
+		//\’z‚Æ”jŠü
+		Model3(const shared_ptr<Stage>& StagePtr, const Vec3& StartPos, const Vec3& StartRot);
+		virtual ~Model3();
+		//‰Šú‰»
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+	};
 
 }
 //end basecross
