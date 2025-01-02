48bf2e468ac8e5dee8add502623371b1137e5108
Tomoki Yoshida <zeal23300042@fsgcl.jp>
2025-01-02 15:56:55 +0900

    TextureとCppとhを追加した

---
+++ GameSources/Actor.cpp
@@ -0,0 +1,38 @@
+/*!
+@file Actor.cpp
+@brief PlayerやEnemyなど動くオブジェクトの親クラス
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	Actor::Actor(shared_ptr<Stage>& stagePtr) :
+		GameObject(stagePtr)
+	{
+
+	}
+
+	Actor::~Actor()
+	{
+
+	}
+
+	float Actor::GetAngle()
+	{
+		return m_angle;
+	}
+
+	void Actor::SetAngle(float angle)
+	{
+		m_angle = angle;
+	}
+
+	void Actor::MoveSwich(bool onoff)
+	{
+		m_move = onoff;
+	}
+
+
+}
+//end basecross
