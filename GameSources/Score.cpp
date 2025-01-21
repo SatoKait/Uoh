eff8d60cb9e1f8b9ee9c799a90984f84727b385c
Chinami Ando <zeal23420147@fsgcl.jp>
2025-01-21 11:50:43 +0900

    NPCの不要なUIを非表示&色のテクスチャを追加&勝ったときと負けたときのアイコンを追加

--- GameSources/Score.cpp
+++ GameSources/Score.cpp
@@ -188,12 +188,12 @@ namespace basecross {
 		{
 			Count++;
 			m_Hp_now = App::GetApp()->GetScene<Scene>()->SetScore2(m_Reset);
-			auto Kakeru = stage->AddGameObject<StageSprite>(L"KAKERU_TX", true,
-				Vec2(64.0f, 64.0f), Vec2(-460.0f, -360.0f));
-			if (m_isLayerBackGroundFlag)
-			{
-				Kakeru->SetDrawLayer(-100);
-			}
+			//auto Kakeru = stage->AddGameObject<StageSprite>(L"KAKERU_TX", true,
+			//	Vec2(64.0f, 64.0f), Vec2(-460.0f, -360.0f));
+			//if (m_isLayerBackGroundFlag)
+			//{
+			//	Kakeru->SetDrawLayer(-100);
+			//}
 		}
 	}
 }
