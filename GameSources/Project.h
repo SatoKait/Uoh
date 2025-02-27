0c920b56bb1f76940d332bd479abe069b6e09de3
Tomoki Yoshida <zeal23300042@fsgcl.jp>
2025-02-27 12:59:04 +0900

    要らないところの削除

--- GameSources/Project.h
+++ GameSources/Project.h
@@ -1,6 +1,7 @@
 /*!
 @file Project.h
 @brief コンテンツ用のヘッダをまとめる
+@authors SatouKaito YoshidaTomoki AndouTinami
 */
 
 #pragma once
@@ -38,15 +39,9 @@
 #include "UITime.h"
 #include "GameOverStage.h"
 #include "FloatCircle.h"
-//#include "Wave.h"
-#include "D3DApp.h"
-//#include "Startcamera.h"
-//#include "MoveCamera.h"
-#include "Gate.h"
 #include "NPC.h"
 #include "OpeningCamera.h"
 #include "MyCamera.h"
 #include "MiniMapActor.h"
 #include "MiniMapManager.h"
-#include "Shader.h"
 #include  "NumberSquare.h"
